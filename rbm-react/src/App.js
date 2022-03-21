5c64ca6635a6540938ad01e77b6f1bf37e44ae28
Andre Ducheylard <ducheylard.andre@gmail.com>
2022-03-21 14:02:33 -0400

    API

--- rbm-react/src/App.js
+++ rbm-react/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    /*<div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,8 +18,64 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </div>*/
+    <>
+  <img src="img/logo+slogan.svg" className="logo" />
+  <div className="greenRectangle">
+    <div className="iniciarSesion">INICIAR SESIÓN</div>
+    <input type="image" src="img/googleButton.svg" className="googleButton" />
+    <p className="terminosCondiciones">
+      Al <strong>Iniciar Sesión</strong>, aceptas nuestros
+      <br />
+      <strong>
+        <a
+          href="pages/terminosCondiciones.html"
+          style={{ textDecoration: "none" }}
+          target="_blank"
+        >
+          Términos y Condiciones
+        </a>
+      </strong>
+      .
+    </p>
+  </div>
+</>
   );
 }
 
 export default App;
+
+/*
+import React from "react";
+import ReactDOM from 'react-dom';
+
+class MainPage extends React.Component {
+  render() {
+  return(
+<>
+  <img src="img/logo+slogan.svg" className="logo" />
+  <div className="greenRectangle">
+    <div className="iniciarSesion">INICIAR SESIÓN</div>
+    <input type="image" src="img/googleButton.svg" className="googleButton" />
+    <p className="terminosCondiciones">
+      Al <strong>Iniciar Sesión</strong>, aceptas nuestros
+      <br />
+      <strong>
+        <a
+          href="pages/terminosCondiciones.html"
+          style={{ textDecoration: "none" }}
+          target="_blank"
+        >
+          Términos y Condiciones
+        </a>
+      </strong>
+      .
+    </p>
+  </div>
+</>
+  );
+  }
+}
+
+ReactDOM.render(<MainPage />, document.getElementById('app'))*/
+//export default MainPage;
